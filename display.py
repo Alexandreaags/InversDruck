a83d565a5e5b89f465d12c11829eacc15ed413e2
“Tassio <tassio-neves@hotmail.com.br>
2023-07-27 14:07:04 +0200

    add display file for reading pico data on pc

---
+++ display.py
@@ -0,0 +1,29 @@
+# Code used to display some Raspberry Pi Pico data on PC
+
+import serial
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+az = []
+
+samples = 2000
+
+### open a serial connection
+pico = serial.Serial("COM3", 115200)
+
+### get Acceleration array
+for i in range(0,samples):
+    inst_acc_z = str(pico.readline())
+    inst_acc_z = int(inst_acc_z[2:-3])
+    az.append(inst_acc_z)
+
+np_az = np.array(az)
+
+samples_array = [range(0, samples)]
+
+plt.plot(np_az, samples_array)
+plt.xlabel('Acceleretion on Z [g]')
+plt.ylabel('Samples')
+
+plt.show()
