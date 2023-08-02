badf90ee3a161563b12e968abacf0703e1ffbdca
“Tassio <tassio-neves@hotmail.com.br>
2023-08-02 18:20:36 +0200

    corrected controller code

--- display.py
+++ display.py
@@ -7,8 +7,7 @@ import matplotlib.pyplot as plt
 
 az = []
 
-samples = 5000
-samples_array = []
+samples = 1000
 
 ### open a serial connection
 pico = serial.Serial("COM6", 115200)
@@ -21,18 +20,16 @@ for i in range(0,samples):
     inst_acc_z = float(inst_acc_z[2:-6])
     print(inst_acc_z)
     az.append(inst_acc_z)
-    samples_array.append(i)
 
 tempo_final = time.time()
 
 np_az = np.array(az)
 
+print(1000/(tempo_final - tempo_inicial))
 
-print(len(samples_array))
-print(5000/(tempo_final - tempo_inicial))
-
-plt.plot(samples_array, np_az)
+plt.plot(range(len(np_az)), np_az)
 plt.xlabel('Samples')
 plt.ylabel('Acceleretion on Z [g]')
+plt.ylim(-5, 5)
 
 plt.show()
