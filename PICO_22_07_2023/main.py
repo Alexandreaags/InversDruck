feccd96286b16aa7bedee37ef5e7843586361923
“Tassio <tassio-neves@hotmail.com.br>
2023-08-02 18:23:11 +0200

    corrected controller

--- PICO_22_07_2023/main.py
+++ PICO_22_07_2023/main.py
@@ -1,25 +1,30 @@
 from imu import MPU6050
-import machine
-from machine import I2C, Pin, UART
-import time
+from time import sleep
+from machine import Pin, I2C
 
+p_up = Pin(16, Pin.OUT)       # 1 for on / 0 for off
+p_down = Pin(17,Pin.OUT) # 1 for up / 0 for down
 
-
-# need this UART to read from BME and be able to send data to local computer
-uart = machine.UART(0, baudrate=115200)
-uart.init(115200, bits=8, parity=None, stop=1, tx=Pin(0), rx=Pin(1))
-
-i2c=I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 imu = MPU6050(i2c)
 
-while True:
-    #ax=round(imu.accel.x,2)
-    #ay=round(imu.accel.y,2)
-    #az=round(imu.accel.z,2)
-    az = imu.accel.z
+threshold = 0.10
 
+while True:
+    #az = 0
+    #for i in range(50):
+    az = imu.accel.z
+    az = abs(az)#/50
+    az = az - 1.13
+    if az < - threshold:
+        p_up.value(0)
+        p_down.value(1)
+    elif az > threshold:
+        p_up.value(1)
+        p_down.value(0)
+    else:
+        p_up.value(0)
+        p_down.value(0)
+
+    #print("az",az, "g\t  ", p_up.value(), p_down.value(),"  ,end="\r")
     
-    #uart.write(str(az))
-    
-    print(str(az))
-    #time.sleep(0.005)
